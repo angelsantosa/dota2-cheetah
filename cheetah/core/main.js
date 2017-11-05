13f36908056b7048aa296187fc6f952d0f416cca
santosa <angel_santos@outlook.com>
2017-11-05 02:38:42 -0600

    main application with single index page

---
+++ cheetah/core/main.js
@@ -0,0 +1,22 @@
+function dragMove(){
+    overwolf.windows.getCurrentWindow(function(result){
+        if (result.status=="success"){
+            overwolf.windows.dragMove(result.window.id);
+        }
+    });
+};
+
+function closeWindow(){
+    overwolf.windows.getCurrentWindow(function(result){
+        if (result.status=="success"){
+            overwolf.windows.close(result.window.id);
+        }
+    });
+};
+function dragResize(edge){
+				overwolf.windows.getCurrentWindow(function(result){
+					if (result.status=="success"){
+						overwolf.windows.dragResize(result.window.id, edge);
+					}
+				});
+			};
