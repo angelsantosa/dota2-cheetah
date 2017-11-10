ce4d265e36bd157d47ba7f54352c2ab9b6c0caab
santosa <angel_santos@outlook.com>
2017-11-09 21:36:13 -0600

    events on click in main

--- cheetah/js/main.js
+++ cheetah/js/main.js
@@ -31,21 +31,44 @@ var Views = {
     data:{}
   }
 }
+
 var cats_nested, cats_json;
+//viws
 var Header = new CheetahTplManager(Views.header);
 var StatusBar = new CheetahTplManager(Views.footer);
 var CatMenu = new CheetahTplManager(Views.main);
+//data
 var CommandList = new CheetahJSONManager(commands_json);
 
 
+
 $(document).ready(function() {
-  //render headerd and status bar
-  //.commands{.name == "gold" }
   cats_nested = CommandList.getNestedObject(".categories");
   cats_json = CommandList.objToJSON(cats_nested);
-
   Header.render();
   StatusBar.render();
   CatMenu.renderData(cats_nested);
+
+});
+
+$(document).on( "click", "#cat", function() {
+  console.log("hi");
+  var catname = $(this).attr('cat');
+  var catid = $(this).attr('catid');
+  var lecommands = CommandList.getNestedObject('.commands{.cat == '+ catid +'}');
+  Views.cat = {
+    tagname: "cateiner",
+    el: $("#cateiner"),
+    template: "templates/"+catname+".html",
+    data:{
+      cat:{
+        id: catid,
+        name: catname
+      },
+      commands: lecommands
+    }
+  }
+  var CatContainer = new CheetahTplManager(Views.cat);
+  CatContainer.render();
 
 });
