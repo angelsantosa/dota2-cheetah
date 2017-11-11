e64f77f9ac5bba3a12cb6d990adcd5fd1fdd7408
santosa <angel_santos@outlook.com>
2017-11-11 01:40:26 -0600

    core major
    -func to clean strings
    -objtojson now a func
    -check if el exists then passes a fun
    -sort for objects in json manager
    -dotadatamanager class created, manages json form dota 2 api and creates a custom matcher for select2

--- cheetah/js/core.js
+++ cheetah/js/core.js
@@ -1,91 +1,222 @@
-class CheetahTplManager{
-  constructor(data){
+function stripDiacritics(text) {
+  return text.normalize('NFD').replace(/[\u0300-\u036f]/g, "");
+}
+
+function sortByField(obj, field) {
+  var ob = obj;
+  var ob = ob.sort(function(a, b) {
+    var an = a[field].toLowerCase();
+    var bn = b[field].toLowerCase();
+    if (an < bn) {
+      return -1;
+      console.log(an + ">" + bn);
+    } else {
+      return 1;
+    }
+  });
+  return ob;
+
+}
+
+function objToJSON(obj) {
+  return JSON.stringify(obj);
+}
+
+class CheetahTplManager {
+  constructor(data) {
     this._tpldata = data;
     this._tpl_el = data.el;
     this._tpl_template = data.template;
     this._tpl_data = data.data;
     this._tpl_tagname = data.tagname;
   }
-  getData(){
+  getData() {
     return this._tpl_data;
   }
-  setData(data){
+  setData(data) {
     this._tpl_data = data;
   }
-  renderData(data){
+  renderData(data) {
     this.setData(data);
     this.render();
   }
-  render(){
+  render(checker,fun, ms=20, select2class=".select2-hidden-accessible") {
+
+    function _do(){
+      var clax = checker.concat(select2class);
+      console.log(clax);
+      var c = $(clax).length;
+      console.log(c);
+      if(c){
+        clearInterval(_loop);
+      }else fun()
+    }
+
     var el = this._tpl_el;
     var tag = this._tpl_tagname;
     var data = this._tpl_data;
-    $.Mustache.load(this._tpl_template).done(function(){
-      el.mustache(tag, data, { method: 'html' });
+    $.Mustache.load(this._tpl_template).done(function() {
+      return el.mustache(tag, data, {
+        method: 'html'
+      });
     });
+    if(checker!=undefined){
+      var _loop = setInterval(_do,ms);
+    }
+
   }
 }
 
-class CheetahJSONManager{
+class CheetahJSONManager {
 
-  constructor(json_file){
+  constructor(json_file) {
     this._json_file = json_file;
     this._object = {};
     this.setObject();
   }
 
-  setObject(){
+  setObject() {
     var these = this;
     var file = this._json_file;
-      $.getJSON(file, function (d) {
-        these._object = d;
-      });
+    $.getJSON(file, function(d) {
+      these._object = d;
+    });
   }
 
-  getObject(pathexp){
-      if(pathexp==undefined){
-        return this._object;
-      }else{
-        return JSPath.apply(pathexp,this._object);
-      }
+  getObject(pathexp) {
+    if (pathexp == undefined) {
+      return this._object;
+    } else {
+      return JSPath.apply(pathexp, this._object);
+    }
   }
 
-  getNestedObject(pathexp){
+  getNestedObject(pathexp) {
     var nst_object = {};
     var file = this._json_file;
     var these = this;
-    var first,who,key;
+    var first, who, key;
     var obj = this.getObject(pathexp);
 
-    if(pathexp == undefined){
+    if (pathexp == undefined) {
       return this._object;
-    }else {
-      $.each(obj, function (k,v) {
-        if(key!=k){
-          who=undefined;
-          key=undefined;
-          first= true;
+    } else {
+      $.each(obj, function(k, v) {
+        if (key != k) {
+          who = undefined;
+          key = undefined;
+          first = true;
         }
-        $.each(v,function (n,m) {
-          if(first){
+        $.each(v, function(n, m) {
+          if (first) {
             key = k;
-            nst_object[m.toString().toLowerCase()]={};
-            nst_object[m.toString().toLowerCase()][n]=m;
+            nst_object[m.toString().toLowerCase()] = {};
+            nst_object[m.toString().toLowerCase()][n] = m;
             first = false;
             who = m.toString().toLowerCase();
-          }else{
-            nst_object[who][n]=m;
+          } else {
+            nst_object[who][n] = m;
           }
         });
 
       });
-    return nst_object;
+      return nst_object;
     }
 
   }
 
-  objToJSON(obj){
-    return JSON.stringify(obj);
+}
+
+class CheetahDota2DataManger {
+
+  constructor(obj, api) {
+    this._api = api;
+    this._obj = this.setObject(obj);
+  }
+
+  getImage(text, suff = "lg") {
+    return "http://cdn.dota2.com/apps/dota2/images/" + this._api + "/" + text + "_" + suff + ".png";
+  }
+
+  setObject(ob) {
+    /*
+    sets image url and shortname
+    */
+    var self = this;
+    var obj = $.map(ob, function(o) {
+      o.simple_name = self.getRawName(o.name);
+      o.image_url = self.getImage(o.simple_name);
+      return o;
+    });
+    return obj;
+  }
+
+  getObjectSelect2() {
+    var self = this;
+    var original_object = self._obj;
+    var new_data = $.map(original_object, function(o) {
+      o.text = o.text || o.localized_name;
+      return o;
+    });
+    return new_data;
+  }
+
+  getRawName(text) {
+    var ntext = text.replace("npc_dota_hero_", "").replace("item_", "");
+    return ntext;
+  }
+
+  getTemplateResult(data) {
+    if (!data.id) {
+      return data.text;
+    }
+    var $state = $(
+      '<span><img width="50" src="' + data.image_url + '" class="img-flag" />' + data.text + '</span>'
+    );
+    return $state;
+  }
+
+  getMatcher(query, data) {
+    /*
+    This matcher doesnt allow childs, don't think ill implement it
+    mostly depends on how easy or hard teh shops is to read
+     */
+    if ($.trim(query.term) === '') {
+      return data;
+    }
+
+    if (typeof data.text === 'undefined' || data.text == "") {
+      return null;
+    }
+    var i;
+
+    var original = stripDiacritics(data.text.toUpperCase());
+    var term = stripDiacritics(query.term.toUpperCase());
+    var term_count = term.length;
+
+    var abbs = data.abb;
+    var abbs_count = abbs.length;
+    var done = null;
+    /*
+    I could have pushed original to abbs but in this way it shows the
+    results for hero names first, the abbs, this is a good way to move data later tho
+    */
+    if (term_count <= 2 && original[0] != term[0]) {
+      return done;
+    }
+    if (original.indexOf(term) > -1) {
+      return data;
+    } else {
+      for (i = 0; i < abbs_count; i++) {
+        var upperabb = stripDiacritics(abbs[i].toUpperCase());
+        if (upperabb.indexOf(term) > -1) {
+          done = data;
+          break;
+        }
+      }
+      return done;
+    }
+    return done;
   }
 
 }
