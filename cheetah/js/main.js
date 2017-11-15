f0cd4c49dcae4aa43d22ce1251924c55e6c1f87b
Angel Santos <angel_santos@outlook.com>
2017-11-15 13:44:18 -0600

    render select as needed in pages

--- cheetah/js/main.js
+++ cheetah/js/main.js
@@ -1,5 +1,6 @@
 var commands_json = "/cheetah/fixtures/commands.json";
 var heroes_json = "/cheetah/fixtures/heroes_merge.json";
+var items_json = "/cheetah/fixtures/items_merge.json"
 var Views = {
   "header": {
     tagname: "header",
@@ -41,6 +42,7 @@ var CatMenu = new CheetahTplManager(Views.main);
 //data
 var CommandData = new CheetahJSONManager(commands_json);
 var HeroData = new CheetahJSONManager(heroes_json);
+var ItemData = new CheetahJSONManager(items_json);
 
 
 $(document).ready(function() {
@@ -74,22 +76,30 @@ $(document).on("click", "#cat", function() {
   var CatContainer = new CheetahTplManager(Views.cat);
 
   var heroes_clear = HeroData.getObject();
+  var items_clear = sortByField(ItemData.getObject(".{.cat==1}"),"cost");
+  var items_extra_clear = sortByField(ItemData.getObject(".{.cat==2}"),"localized_name");
+
   var heroes_data = new CheetahDota2DataManger(heroes_clear, "heroes");
+  var itmes_data = new CheetahDota2DataManger(items_clear, "items");
+  var itmes_extra_data = new CheetahDota2DataManger(items_extra_clear, "items");
 
   CatContainer.render();
   //rendering select on cat
-  switch (catid) {
+  switch (parseInt(catid)) {
     case cats_nested.general.id:
-
-      break;
-
-    case cats_nested.world.id:
-
+      var items_select = new CheetahSelect2Manager("#gen_items", ".gen_items", itmes_data, "Select an item");
+      var items_extra_select = new CheetahSelect2Manager("#gen_items_extra", ".gen_items_extra", itmes_extra_data, "Select an item");
+      items_select.build();
+      items_extra_select.build();
       break;
 
     case cats_nested.spawns.id:
-      var heroes_select = new CheetahSelect2Manager("#heroes", ".heroes", heroes_data, "Select a hero");
+      var heroes_select = new CheetahSelect2Manager("#spwn_heroes", ".spwn_heroes", heroes_data, "Select a hero");
+      var items_select = new CheetahSelect2Manager("#spwn_items", ".spwn_items", itmes_data, "Select an item");
+      var items_extra_select = new CheetahSelect2Manager("#spwn_items_extra", ".spwn_items_extra", itmes_extra_data, "Select an item");
       heroes_select.build();
+      items_select.build();
+      items_extra_select.build();
   }
 
 });
